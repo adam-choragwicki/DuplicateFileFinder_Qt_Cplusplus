42766cefa4a50007e9220532e81bcfbfa0e7d54a
Adam Choragwicki <adam.choragwicki.dev@gmail.com>
2026-08-02 21:27:35 +0200

    Add FileRecord::getAbsoluteFilePath Add FileRecord::getLastModified

--- src/types/file_record.h
+++ src/types/file_record.h
@@ -1,21 +1,27 @@
 #pragma once
 
 #include <QDir>
+#include <QDateTime>
 
 class FileRecord
 {
 public:
-    FileRecord(const QString& fileName, const QString& directoryPath, const qint64 sizeBytes)
-        : fileName_(fileName), directoryPath_(directoryPath), sizeBytes_(sizeBytes)
+    FileRecord(QString fileName, QString directoryPath, const qint64 sizeBytes, QDateTime lastModified = {})
+        : fileName_(std::move(fileName)),
+          directoryPath_(std::move(directoryPath)),
+          sizeBytes_(sizeBytes),
+          lastModified_(std::move(lastModified))
     {}
 
     [[nodiscard]] const QString& getFileName() const { return fileName_; }
     [[nodiscard]] const QString& getDirectoryPath() const { return directoryPath_; }
     [[nodiscard]] QString getAbsoluteFilePath() const { return QDir(directoryPath_).filePath(fileName_); }
     [[nodiscard]] qint64 getSizeBytes() const { return sizeBytes_; }
+    [[nodiscard]] const QDateTime& getLastModified() const { return lastModified_; }
 
 private:
     QString fileName_;
     QString directoryPath_;
     qint64 sizeBytes_;
+    QDateTime lastModified_;
 };
