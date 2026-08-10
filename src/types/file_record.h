5665a6105bfad4c4697e28d6a2a35701608facb8
Adam Choragwicki <adam.choragwicki.dev@gmail.com>
2026-08-10 20:11:24 +0200

    Clean up

--- src/types/file_record.h
+++ src/types/file_record.h
@@ -1,27 +1,23 @@
 #pragma once
 
 #include <QDir>
-#include <QDateTime>
 
 class FileRecord
 {
 public:
-    FileRecord(QString fileName, QString directoryPath, const qint64 sizeBytes, QDateTime lastModified = {})
+    FileRecord(QString fileName, QString directoryPath, const qint64 sizeBytes)
         : fileName_(std::move(fileName)),
           directoryPath_(std::move(directoryPath)),
-          sizeBytes_(sizeBytes),
-          lastModified_(std::move(lastModified))
+          sizeBytes_(sizeBytes)
     {}
 
     [[nodiscard]] const QString& getFileName() const { return fileName_; }
     [[nodiscard]] const QString& getDirectoryPath() const { return directoryPath_; }
     [[nodiscard]] QString getAbsoluteFilePath() const { return QDir(directoryPath_).filePath(fileName_); }
     [[nodiscard]] qint64 getSizeBytes() const { return sizeBytes_; }
-    [[nodiscard]] const QDateTime& getLastModified() const { return lastModified_; }
 
 private:
     QString fileName_;
     QString directoryPath_;
     qint64 sizeBytes_;
-    QDateTime lastModified_;
 };
