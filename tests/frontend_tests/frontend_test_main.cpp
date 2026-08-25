2b2bc10914371ec757052de0cbbf77b12f7219b6
Adam Choragwicki <adam.choragwicki.dev@gmail.com>
2026-08-25 17:59:03 +0200

    Add environment variable QT_QPA_PLATFORM=offscreen to make GUI tests headless

--- tests/frontend_tests/frontend_test_main.cpp
+++ tests/frontend_tests/frontend_test_main.cpp
@@ -4,6 +4,9 @@
 
 int main(int argc, char* argv[])
 {
+    // Prevent frontend tests from creating native windows, regardless of how the test executable is launched.
+    qputenv("QT_QPA_PLATFORM", QByteArrayLiteral("offscreen"));
+
     QApplication application(argc, argv);
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
